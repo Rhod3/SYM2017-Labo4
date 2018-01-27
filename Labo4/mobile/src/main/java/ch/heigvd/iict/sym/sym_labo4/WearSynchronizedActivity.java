cb06e3620052142fa9281c6ae330fb6f5a49a8d5
Nicolas Rod <ncls.rod@gmail.com>
2018-01-27 17:47:03 +0100

    Done

---
+++ Labo4/mobile/src/main/java/ch/heigvd/iict/sym/sym_labo4/WearSynchronizedActivity.java
@@ -0,0 +1,80 @@
+package ch.heigvd.iict.sym.sym_labo4;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.google.android.gms.wearable.DataClient;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataItem;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.Wearable;
+
+public class WearSynchronizedActivity extends AppCompatActivity implements DataClient.OnDataChangedListener {
+
+    private static final String TAG = WearSynchronizedActivity.class.getSimpleName();
+
+    // Inspired from https://developer.android.com/training/wearables/data-layer/data-items.html
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wearsynchronized);
+
+        Wearable.getDataClient(getApplicationContext()).addListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Wearable.getDataClient(getApplicationContext()).addListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Wearable.getDataClient(getApplicationContext()).removeListener(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Wearable.getDataClient(getApplicationContext()).removeListener(this);
+    }
+
+    @Override
+    public void onDataChanged(@NonNull DataEventBuffer dataEventBuffer) {
+        for (DataEvent event : dataEventBuffer) {
+            if (event.getType() == DataEvent.TYPE_CHANGED) {
+                DataItem item = event.getDataItem();
+
+                if (item.getUri().getPath().compareTo("/colors") == 0) {
+                    DataMap map = DataMapItem.fromDataItem(item).getDataMap();
+                    updateColor(map.getInt("R"),
+                            map.getInt("G"),
+                            map.getInt("B"));
+                }
+            }
+        }
+    }
+
+    /*
+     *  Code utilitaire fourni
+     */
+
+    /**
+     * Method used to update the background color of the activity
+     * @param r The red composant (0...255)
+     * @param g The green composant (0...255)
+     * @param b The blue composant (0...255)
+     */
+    private void updateColor(int r, int g, int b) {
+        View rootView = findViewById(android.R.id.content);
+        rootView.setBackgroundColor(Color.argb(255, r,g,b));
+    }
+
+}
