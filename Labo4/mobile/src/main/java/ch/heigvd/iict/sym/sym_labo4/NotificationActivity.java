e3564d5252d85b64175f39ac1b12692a1173b105
Nadir Benallal <nadir.benallal@heig-vd.ch>
2018-01-28 14:55:06 +0100

    Refactor + commenting Notification.java

--- Labo4/mobile/src/main/java/ch/heigvd/iict/sym/sym_labo4/NotificationActivity.java
+++ Labo4/mobile/src/main/java/ch/heigvd/iict/sym/sym_labo4/NotificationActivity.java
@@ -1,6 +1,5 @@
 package ch.heigvd.iict.sym.sym_labo4;
 
-import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,6 +8,7 @@ import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -16,12 +16,28 @@ import ch.heigvd.iict.sym.wearcommon.Constants;
 
 public class NotificationActivity extends AppCompatActivity {
 
+    // Button to send a pending intent notification
     private Button pendingIntent;
+
+    // Button to send notification with action buttons
     private Button action;
+
+    // Button to send a notification with wearable only features
     private Button wearableOnly;
 
+    //Notification builder used to build the notification
     private NotificationCompat.Builder notificationBuilder;
 
+    // Notification manager used to send the notification
+    NotificationManagerCompat notificationManager;
+
+    /**
+     * Called at the creation of the activity.
+     * In this phase we initialise the buttons of the activity and bind
+     * a OnClickListener to each one of them.
+     *
+     * @param savedInstanceState the previous saved state of the activity
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,43 +46,57 @@ public class NotificationActivity extends AppCompatActivity {
         if (getIntent() != null)
             onNewIntent(getIntent());
 
+        // Initialisation of the buttons
         pendingIntent = findViewById(R.id.notif_btn_pending);
         action = findViewById(R.id.notif_btn_action);
         wearableOnly = findViewById(R.id.notif_btn_wearable_only);
 
-        pendingIntent.setOnClickListener(new View.OnClickListener() {
+        // Initialisation of the notification manager
+        notificationManager = NotificationManagerCompat.from(this);
+
+        // Binding the click listeners to the buttons
+        pendingIntent.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                pendingIntentNotification();
+                notifyPendingIntent();
             }
         });
 
-        action.setOnClickListener(new View.OnClickListener() {
+        action.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                actionNotification();
+                notifyActionButton();
             }
         });
 
-        wearableOnly.setOnClickListener(new View.OnClickListener() {
+        wearableOnly.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                wearableOnlyNotification();
+                notifyWearableOnly();
             }
         });
     }
 
-    private void pendingIntentNotification() {
+    /**
+     * Creating and sending a pending intent notification.
+     */
+    private void notifyPendingIntent() {
+        // Building the pending intent notification
         notificationBuilder = new NotificationCompat.Builder(this, getString(R.string.pending_id))
                 .setSmallIcon(R.drawable.ic_lightbulb_on_white_18dp)
                 .setContentTitle(getString(R.string.pending_title))
                 .setContentText(getString(R.string.pending_text))
                 .setContentIntent(createPendingIntent(0, getString(R.string.toast_phone)));
 
-        notify(notificationBuilder.build());
+        // Sending the notification
+        notificationManager.notify(1, notificationBuilder.build());
     }
 
-    private void actionNotification() {
+    /**
+     * Creating and sending a notification with an action button.
+     */
+    private void notifyActionButton() {
+        // Building the notification with an action button
         notificationBuilder = new NotificationCompat.Builder(this, getString(R.string.action_id))
                 .setSmallIcon(R.drawable.ic_lightbulb_on_white_18dp)
                 .setContentTitle(getString(R.string.action_title))
@@ -74,13 +104,18 @@ public class NotificationActivity extends AppCompatActivity {
                 .addAction(R.drawable.ic_lightbulb_on_white_18dp, getString(R.string.action_button_text),
                         createPendingIntent(0, getString(R.string.toast_phone)));
 
-        notify(notificationBuilder.build());
+        // Sending the notification
+        notificationManager.notify(1, notificationBuilder.build());
     }
 
-    private void wearableOnlyNotification() {
+    /**
+     * Creating and sending a notification with wearable only features.
+     */
+    private void notifyWearableOnly() {
         NotificationCompat.WearableExtender wearableExtender =
                 new NotificationCompat.WearableExtender().setHintHideIcon(true);
 
+        // Building the notification with wearable only features
         notificationBuilder = new NotificationCompat.Builder(this, getString(R.string.wearable_only_id))
                 .setSmallIcon(R.drawable.ic_lightbulb_on_white_18dp)
                 .setContentTitle(getString(R.string.wearable_only_title))
@@ -89,13 +124,15 @@ public class NotificationActivity extends AppCompatActivity {
                 .addAction(R.drawable.ic_lightbulb_on_white_18dp, getString(R.string.action_button_text),
                         createPendingIntent(0, getString(R.string.toast_phone)));
 
-        notify(notificationBuilder.build());
+        // Sending the notification
+        notificationManager.notify(1, notificationBuilder.build());
     }
 
-    private void notify(Notification notification) {
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-        notificationManager.notify(1, notification);
-    }
+    /*
+    --------------------------
+    Fourni dans le template
+    --------------------------
+     */
 
     /*
      *  Code fourni pour les PendingIntent
